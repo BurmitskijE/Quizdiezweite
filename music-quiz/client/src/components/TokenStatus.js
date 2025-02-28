1d7af8501061a4565ec1fdce0ca7b9a195afc158
Eric Burmitskij <124143025+BurmitskijE@users.noreply.github.com>
2025-02-28 13:26:12 +0000

    Add styling to TokenStatus component and create associated CSS file

--- music-quiz/client/src/components/TokenStatus.js
+++ music-quiz/client/src/components/TokenStatus.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/TokenStatus.css"; // Import the CSS file
 
 const TokenStatus = ({ accessToken }) => {
   return (
-    <div>
+    <div className="token-status">
       <p>🔑 Eingeloggt als: {accessToken}</p>
       <button onClick={() => window.location.reload()}>🔄 Refresh Token</button>
     </div>
